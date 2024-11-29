3ab94523ad79f89953e3b5981cf4c5d42d69dd50
Maja-co <onlineshit61@gmail.com>
2024-11-29 08:54:49 +0100

    Nyeste update 29/11 - DEltagerliste opdateres ikke - Mangler en samlet pris i popUp vinduet

--- src/Gui/App.java
+++ src/Gui/App.java
@@ -9,51 +9,45 @@ import java.time.LocalDate;
 public class App {
     public static void main(String[] args) {
         initStorage();
-        Application.launch(MainWindow.class);
 
-// Test 01
-        /*
-        System.out.println("Test 01 ");
-        Participant finnMadsen = Controller.createParticipant("Finn Madsen", "Bysmedvej 24","Denmark", "31313131");
-
-        Conferences havOgHimmel = Controller.createConference("Hav og Himmel", LocalDate.of(2024, 1, 10), LocalDate.of(2024, 1, 13), "Island Brygge", 1500);
-
-        Hotel denHvideSvane = Controller.createHotel("Den Hvide Svane","Glemsomhedens sti 42",1050, 1250);
-
-        Enrollment enrollment1 = Controller.createEnrollment(true,false,false,false,LocalDate.of(2024,1,10), LocalDate.of(2024,1,13), "Finn Madsen",havOgHimmel, denHvideSvane);
-
-
-        System.out.println(enrollment1.calculateTotalPrice(enrollment1));
-        */
-        //Test 02
-        /*
-        System.out.println("Test 02 ");
-
-        Participant nielsPetersen = Controller.createParticipant("Niels Petersen", "Fuldstændig ligegyldigt", "Afrikaaaaa", "31313131");
-        Enrollment enrollment2 = Controller.createEnrollment(false,false,true,false,LocalDate.of(2024,1,10), LocalDate.of(2024,1,13),"Niels Petersen",havOgHimmel,denHvideSvane);
-
-        System.out.println(enrollment2.calculateTotalPrice(enrollment2));
-        */
-
-        //Test 03
-        /*
-        System.out.println("Test 03 ");
-
-        Participant peterSommer = Controller.createParticipant("Peter Sommer","Bumsestræde 666","Mælkevejen", "31313131");
-        HotelFacilities wifi = Controller.createHotelFacilities("Wifi",50);
-        Event egeskov = Controller.createEvent("Egeskov", "GGWP", LocalDate.of(2024,12,19),LocalDate.of(2024,12,19),75);
-        Event trapHolt = Controller.createEvent("Trapholt", "GGWP", LocalDate.of(2024,12,20),LocalDate.of(2024,12,20),200);
-
-        Enrollment enrollment3 = Controller.createEnrollment(true,true,true,false,LocalDate.of(2024,12,10), LocalDate.of(2024,12,13),"Peter Sommer",havOgHimmel,denHvideSvane);
-        System.out.println(enrollment3.calculateTotalPrice(enrollment3));
-        */
-
-        //Test 04
-//        System.out.println("Test 04");
-//        Participant loneJensen = Controller.createParticipant("Lone Jensen", "ÅåååhGud", "Polen", "31313131");
 //        Conferences havOgHimmel = Controller.createConference("Hav og Himmel", LocalDate.of(2024, 1, 10), LocalDate.of(2024, 1, 13), "Island Brygge", 1500, "Business", 200);
-//
 //        Hotel denHvideSvane = Controller.createHotel("Den Hvide Svane", "Glemsomhedens sti 42", 1000, 1250);
+//
+//// Test 01
+//        System.out.println("Test 01 ");
+//        Participant finnMadsen = Controller.createParticipant("Finn Madsen", "H.C. Andersens Boulevard 34, 1. th, 1553 København V","Denmark", "+45 88707270");
+//        Enrollment enrollment01 = Controller.createEnrollment(
+//                true, false, false, false,
+//                LocalDate.of(2024, 1, 10), LocalDate.of(2024, 1, 13),
+//                finnMadsen, havOgHimmel, denHvideSvane);
+//        System.out.println(enrollment01.calculateTotalPrice());
+//
+//        //Test 02
+//        System.out.println("Test 02 ");
+//        Participant nielsPetersen = Controller.createParticipant("Niels Petersen", "Tophøjvej 2B, Brøndum, 9500 Hobro", "Danmark", "+45 72214184");
+//        Enrollment enrollment02 = Controller.createEnrollment(
+//                false, false, true, false,
+//                LocalDate.of(2024, 1, 10), LocalDate.of(2024, 1, 13),
+//                nielsPetersen, havOgHimmel, denHvideSvane);
+//        System.out.println(enrollment02.calculateTotalPrice());
+//
+//
+//        //Test 03
+//        System.out.println("Test 03 ");
+//        Participant peterSommer = Controller.createParticipant("Peter Sommer","Gl. Landevej 34A, Roskilde By, 4000 Roskilde", "Danmark", "+45 95568645");
+//        HotelFacilities wifi = Controller.createHotelFacilities("Wifi",50);
+//        Event egeskov = Controller.createEvent("Egeskov", "GGWP", LocalDate.of(2024,12,19),LocalDate.of(2024,12,19),75);
+//        Event trapHolt = Controller.createEvent("Trapholt", "GGWP", LocalDate.of(2024,12,20),LocalDate.of(2024,12,20),200);
+//
+//        Enrollment enrollment03 = Controller.createEnrollment(
+//                true, true, true, false,
+//                LocalDate.of(2024, 1, 10), LocalDate.of(2024, 1, 13),
+//                peterSommer, havOgHimmel, denHvideSvane);
+//        System.out.println(enrollment03.calculateTotalPrice());
+//
+//        //Test 04
+//        System.out.println("Test 04");
+//        Participant loneJensen = Controller.createParticipant("Lone Jensen", "Vigerslev Allé 158P, Hf. Danshøj, 2500 Valby", "Danmark", "+45 41852843");
 //        HotelFacilities wifi = Controller.createHotelFacilities("Wifi", 50);
 //        Event egeskov = Controller.createEvent("Egeskov", "GGWP", LocalDate.of(2024, 12, 19), LocalDate.of(2024, 12, 19), 75);
 //        Event trapHolt = Controller.createEvent("byrundtur i Odense", "GGWP", LocalDate.of(2024, 12, 20), LocalDate.of(2024, 12, 20), 125);
@@ -63,6 +57,8 @@ public class App {
 //                LocalDate.of(2024, 1, 10), LocalDate.of(2024, 1, 13),
 //                loneJensen, havOgHimmel, denHvideSvane);
 //        System.out.println(enrollment04.calculateTotalPrice());
+
+        Application.launch(MainWindow.class);
     }
 
 
